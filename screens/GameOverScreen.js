f2b959ba0018a6948ecc5f58400ff2b47e239336
iBearArms <tstan2310@gmail.com>
2022-08-09 11:11:25 -0400

    styled and tweaked GameOverScreen, updated react-native package to be compatible with expo version

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,58 @@
-import { Text, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
+
+import Title from "../components/ui/Title";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Colors from "../constants/colors";
 
 function GameOverScreen() {
-  return <Text>Game is over!</Text>;
+  return (
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={require("../images/fight.jpg")} />
+      </View>
+      <View>
+        <Text style={styles.summaryText}>
+          Your phone took <Text style={styles.highlight}>X</Text> rounds to
+          guess the number
+          <Text style={styles.highlight}> Y</Text>.
+        </Text>
+        <PrimaryButton>Start New Game</PrimaryButton>
+      </View>
+    </View>
+  );
 }
 
 export default GameOverScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary400,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
+  },
+});
